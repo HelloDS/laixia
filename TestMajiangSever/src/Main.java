d70e2b25aa0079f8007bbdb44bc861d4c06526f1
HelloDS <1154195328@qq.com>
2018-06-11 16:21:31 +0800

    写不动了  缓存一下

--- TestMajiangSever/src/Main.java
+++ TestMajiangSever/src/Main.java
@@ -1,22 +1,58 @@
 
 import com.laixia.majiang.net.GameTcp;
+import com.laixia.majiang.net.GameTcpClient;
 import com.laixia.majiang.utils.ClassUtils;
 
 import org.apache.log4j.Logger;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
     private static Logger logger = Logger.getLogger(ClassUtils.getCurClass());
 
-    public static void main(String[] args) {
+    private  static GameTcp server = null;
 
-        logger.info("hello,world");
+    private static String hostname = "192.168.79.1";
+    private static int port = 10003;
+    private static String content = "sssss";
+    public static void main(String[] args)
+    {
+        ServerSocket sk= null;
+        try {
+            sk = new ServerSocket(port);
 
-
-        System.out.println("服务器启动...\n");
-        GameTcp server = new GameTcp();
-        server.init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while(true)
+        {
+            Socket s= null;
+            try {
+                s = sk.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            new Thread(new GameTcp(s)).start();
+        }
     }
-
-
-
 }
+
+
+
+//        TimerTask task = new TimerTask() {
+//            @Override
+//            public void run() {
+//                System.out.println("Hello !!!");
+//                if ( server == null) {
+//                    return;
+//                }
+//                else
+//                {
+//                    server.toclent();
+//                }
+//            }
+//        };
--- TestMajiangSever/src/com/laixia/majiang/net/GameTcp.java
+++ TestMajiangSever/src/com/laixia/majiang/net/GameTcp.java
@@ -4,71 +4,42 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-
-import com.alibaba.fastjson.JSONObject;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-
-public class GameTcp {
-    private String hostname;
-    private int port;
-    private String content;
-
-    public GameTcp(String hostname, int port,String content){
-        this.hostname = hostname;
-        this.port = port;
-        this.content = content;
-    }
-
-    public  String Do() throws Exception
-    {
-        String result="";
-        PrintWriter out = null;
-        BufferedReader in = null;
-        try
+public class GameTcp implements Runnable
+{
+        private Socket s;
+        public GameTcp(Socket s)
         {
-            Socket socket = new Socket();
-            //socket链接指定的主机,超过10秒抛出链接不上异常
-            socket.connect(new InetSocketAddress(hostname,port), 10000);
-            // 得到请求的输出流对象
-            out = new PrintWriter(socket.getOutputStream());
-            // 发送请求参数
-            out.print(content);
-            // flush输出流的缓冲
-            out.flush();
-            // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-            System.out.println("获取的结果为："+result);
+            this.s=s;
         }
-        catch(Exception ex)
+
+        public void run()
         {
-            System.out.println("发送 POST 请求出现异常！"+ex);
-            ex.printStackTrace();
-            throw ex;
-        }
-        finally
-        {
-            try{
-                if(out!=null){
-                    out.close();
+            try
+            {
+                System.out.println(s.getInetAddress().getHostAddress()+".......conneted");
+                BufferedReader fr = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                PrintWriter w = new PrintWriter(s.getOutputStream(),true);
+
+
+                StringBuilder builder = new StringBuilder();
+                String line=null;
+                while((line=fr.readLine())!=null)
+                {
+                    builder.append(line);
+                    w.println(line);
+                    System.out.print(builder);
                 }
-                if(in!=null){
-                    in.close();
-                }
+
+                fr.close();
+                //w.close();
             }
-            catch(IOException ex){
-                ex.printStackTrace();
+            catch(Exception e)
+            {
+                System.out.println("服务端错误");
             }
         }
-        return result;
-    }
 }
